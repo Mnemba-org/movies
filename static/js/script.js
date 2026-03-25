57cce2c6dbad04defb22c9b6048ab913d9c57a42
online_application <mnembaorg@gmail.com>
2026-03-25 22:56:03 +0300

    Delete static directory

--- static/js/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('click', function(event) {
-    if (!event.target.closest('.dropdown')) {
-        document.querySelector('.dropdown-content').classList.remove('show');
-    }
-});
